7653f77726e4ff8530e19ed90615cf30908d05d0
debashis nandi <india.debashis@gmail.com>
2017-09-18 00:48:42 +0530

    hibernate 5 changes

--- hb/src/main/java/com/global/hb/ShowList.java
+++ hb/src/main/java/com/global/hb/ShowList.java
@@ -5,6 +5,7 @@
  */
 package com.global.hb;
 
+import java.time.LocalDateTime;
 import java.util.Iterator;
 import java.util.List;
 import org.hibernate.*;
@@ -20,14 +21,15 @@ public class ShowList {
 		Session session1 = HibernateUtil.getSession();
 		session1.beginTransaction();
 
-		Query query = session1.createQuery("FROM AdminLogin");
-		List adminlist = query.list();
-		
-		for (Iterator iterator = adminlist.iterator(); iterator.hasNext();) {
-			AdminLogin adm = (AdminLogin) iterator.next();
+		@SuppressWarnings("unchecked")
+		List<AdminLogin> adminlist = session1.createQuery("FROM com.global.hb.AdminLogin").getResultList();
+
+		for (Iterator<AdminLogin> iterator = adminlist.iterator(); iterator.hasNext();) {
+			AdminLogin adm = iterator.next();
 			System.out.print("\n=================================================================\n");
 			System.out.print(" User ID: " + adm.getUserid());
 			System.out.print(" || Password : " + adm.getPassword());
+			System.out.print(" || DateTime : " + LocalDateTime.now().toString());
 			System.out.print("\n=================================================================\n");
 
 		}
