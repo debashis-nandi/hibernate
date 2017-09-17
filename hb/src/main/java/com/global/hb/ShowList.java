9f944a19f373f39ef75a2db33afa60050e27b1a8
debashis nandi <india.debashis@gmail.com>
2017-09-17 17:51:50 +0530

    add === line

--- hb/src/main/java/com/global/hb/ShowList.java
+++ hb/src/main/java/com/global/hb/ShowList.java
@@ -15,28 +15,27 @@ import org.hibernate.*;
  */
 public class ShowList {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        Session session1 = HibernateUtil.getSession();
-        session1.beginTransaction();
+		Session session1 = HibernateUtil.getSession();
+		session1.beginTransaction();
 
-        Query query = session1.createQuery("FROM AdminLogin");
-        List<AdminLogin> adminlist = query.list();
-        String t = "";
-        for (Iterator<AdminLogin> iterator
-                = adminlist.iterator(); iterator.hasNext();) {
-            AdminLogin adm = iterator.next();
-            System.out.print("\n=================================================================\n");
-            System.out.print(" User ID: " + adm.getUserid());
-            System.out.print(" || Password : " + adm.getPassword());
-             System.out.print("\n=================================================================\n");
-             
-        }
-        
+		Query query = session1.createQuery("FROM AdminLogin");
+		List<AdminLogin> adminlist = query.list();
+		String t = "";
+		for (Iterator<AdminLogin> iterator = adminlist.iterator(); iterator.hasNext();) {
+			AdminLogin adm = iterator.next();
+			System.out.print("\n=================================================================\n");
+			System.out.print(" User ID: " + adm.getUserid());
+			System.out.print(" || Password : " + adm.getPassword());
+			System.out.print("\n=================================================================\n");
+			System.out.print("\n=================================================================\n");
 
-        session1.getTransaction().commit();
+		}
 
-        session1.close();
+		session1.getTransaction().commit();
 
-    }
+		session1.close();
+
+	}
 }
